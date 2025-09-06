f55fc0f79fea877ba9e8fb9dbd2a576d860e092c
thienkk25 <thiennguyendz25@gmail.com>
2025-09-06 13:50:41 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# youtube_mini_video_demo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/youtube_mini_video_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.youtube_mini_video_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
